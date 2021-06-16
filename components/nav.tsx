f6e2865dec89a695015ed3ec2b51fd162b5483fc
CIPHERTron <pritish.samal918@gmail.com>
2021-06-16 21:51:55 +0530

    chore: Add Navbar Component[Desktop]

---
+++ components/nav.tsx
@@ -0,0 +1,60 @@
+import React, { useContext } from 'react';
+import Link from 'next/link';
+
+import { NavSection, StyledHamburger } from '../styles/nav.styles';
+// import Logo from './logo';
+import { Container } from './container';
+import SiteConfig from '../config/index.json';
+import { MenuContext } from '.';
+
+const Nav = () => {
+  const menuContext = useContext(MenuContext);
+
+  const { toggleMenuOpen, menuOpen } = menuContext;
+
+  return (
+    <NavSection>
+      <Container>
+        <nav className='navWrapper'>
+          <div className='navLeft'>
+            <Link href='/'>
+              <a className='no-underline'>
+                {/* <Logo /> */}
+                <span>{SiteConfig.author.name}</span>
+              </a>
+            </Link>
+          </div>
+
+          <div className='navRight'>
+            <StyledHamburger menuOpen={menuOpen} onClick={toggleMenuOpen}></StyledHamburger>
+
+            <ul className='navLinkList'>
+              <li className='navLinkItem'>
+                <Link href='/works'>
+                  <a>Work</a>
+                </Link>
+              </li>
+              <li className='navLinkItem'>
+                <Link href='/articles'>
+                  <a>Blogs</a>
+                </Link>
+              </li>
+              <li className='navLinkItem'>
+                <Link href='/notes'>
+                  <a>Photography</a>
+                </Link>
+              </li>
+              <li className='navLinkItem'>
+                <Link href='/about'>
+                  <a>About</a>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </Container>
+    </NavSection>
+  );
+};
+
+export default Nav;
