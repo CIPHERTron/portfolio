39cfb5aa597ee62343ef1080fdb21d1d8c259acd
CIPHERTron <pritish.samal918@gmail.com>
2021-06-19 03:53:18 +0530

    chore: Add Skills Section

---
+++ components/skils.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Chip, Container } from './.';
+import { StyledSkills, Heading, Wrapper } from '../styles/skills.styles';
+import { skills } from '../config/skills';
+
+const SkillsSection = () => {
+  return (
+    <StyledSkills>
+      <Container width='default'>
+        <Heading>Tech I'm Familiar With</Heading>
+        <Wrapper>
+          {skills.map(({ name, src }, index) => (
+            <Chip key={index} name={name} src={src} />
+          ))}
+        </Wrapper>
+      </Container>
+    </StyledSkills>
+  );
+};
+
+export default SkillsSection;
