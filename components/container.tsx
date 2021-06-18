091106069ebdbaccd8639dc28c32a32c861ec2c6
CIPHERTron <pritish.samal918@gmail.com>
2021-06-19 03:56:27 +0530

    chore: Add Container Which Sets width Acc. to config

---
+++ components/container.tsx
@@ -0,0 +1,17 @@
+import React, { FC, HTMLAttributes } from 'react';
+import { StyledContainer } from '../styles/container.styles';
+import { IWidth } from '../util/getWidth';
+
+export interface IContainer extends HTMLAttributes<HTMLElement> {
+  width?: IWidth;
+}
+
+const Container: FC<IContainer> = ({ children, width = 'default', ...props }) => {
+  return (
+    <StyledContainer width={width} {...props}>
+      {children}
+    </StyledContainer>
+  );
+};
+
+export { Container };
