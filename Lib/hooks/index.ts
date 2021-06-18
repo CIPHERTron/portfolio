113a0c514a275fb42f2807a045e98656e895d194
CIPHERTron <pritish.samal918@gmail.com>
2021-06-19 03:43:51 +0530

    fix: Remove Unnecessary Hooks

--- Lib/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as useInput } from './useInput';
-export { default as useSwitch } from './useSwitch';
-export { default as useToggle } from './useToggle';
-export { default as useCounter } from './useCounter';
-export { default as useAxios } from './useAxios';
--- Lib/hooks/useToggle.ts
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react';
-
-export default (
-  initialState = false,
-): [boolean, () => void, (state: boolean) => void] => {
-  const [state, setState] = useState(initialState);
-  const toggle = (): void => setState(currentState => !currentState);
-  return [state, toggle, setState];
-};
