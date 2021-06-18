e4a3b5b27551ae7c05f80ec1d1716fcd3663dc48
CIPHERTron <pritish.samal918@gmail.com>
2021-06-19 03:56:49 +0530

    chore: Add Container Styling

---
+++ styles/container.styles.ts
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import { IContainer } from '../components/container';
+import { getWidth } from '../util/getWidth';
+
+export const StyledContainer = styled.section<IContainer>`
+  padding: 0 4%;
+  max-width: ${({ width }) => getWidth(width)};
+  margin: 0 auto;
+`;
