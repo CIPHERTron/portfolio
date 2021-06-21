af1966067495ae79abc7e1b5087a45d16ce9a131
CIPHERTron <pritish.samal918@gmail.com>
2021-06-21 21:44:49 +0530

    chore: Dynamically Render Project Desc. md Files

---
+++ pages/works/[id].tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+import { Layout, Container } from '../../components';
+import { getAllContentIds, getContentData } from '../../lib/content';
+import { IContentData } from '../articles/[id]';
+import { StyledContent } from '../../styles/content.styles';
+
+/**
+ *  Renders work markdown posts
+ */
+
+const Article = ({ worksData }) => {
+  const { pathname } = useRouter();
+  const { title, contentHtml, description } = worksData;
+
+  return (
+    <Layout pageTitle={title} pathname={pathname} pageDescription={description}>
+      <Container width='narrow'>
+        <StyledContent>
+          <time>{worksData.date}</time>
+          {worksData.previewImage && (
+            <Image src={worksData.previewImage} height={550} width={1200} />
+          )}
+          <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+        </StyledContent>
+      </Container>
+    </Layout>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const paths = getAllContentIds('work');
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const worksData: IContentData = await getContentData(params.id, 'work');
+  return {
+    props: {
+      worksData,
+    },
+  };
+};
+
+export default Article;
