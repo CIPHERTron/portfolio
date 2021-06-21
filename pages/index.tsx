19de24d7d6ef06beb9b2124ccfe885c871e8f960
CIPHERTron <pritish.samal918@gmail.com>
2021-06-21 21:16:36 +0530

    feat: Add Project & Blog Section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,18 @@
 import React, { FC } from 'react';
 
-import { Layout, AboutSection, ExperimentsSection, SkillsSection, Cards } from '../components';
+import { Layout, AboutSection, Blogs, SkillsSection, Cards, Container } from '../components';
 import { StyledIndexPage } from '../styles/home.styles';
 import { getContentList } from '../lib/content';
 
 //@ts-ignore
-const Index: FC = ({ selectedWorks }) => {
+const Index: FC = ({ selectedWorks, articles }) => {
   return (
     <Layout pathname={'/'} pageTitle='Pritish Samal'>
       <StyledIndexPage>
         <AboutSection />
         <SkillsSection />
         <Cards data={selectedWorks} basePath='works' />
-        <ExperimentsSection />
+        <Blogs articles={articles} basePath='articles' />
       </StyledIndexPage>
     </Layout>
   );
@@ -21,10 +21,12 @@ const Index: FC = ({ selectedWorks }) => {
 export const getStaticProps = async () => {
   const works = await getContentList('work');
   const selectedWorks = works.filter((work) => work.selectedWork);
+  const articles = getContentList('articles');
 
   return {
     props: {
       selectedWorks,
+      articles,
     },
   };
 };
