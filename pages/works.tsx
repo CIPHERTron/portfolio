c0ec76cec7f4d54d99c766f2d3073296a1f1f183
CIPHERTron <pritish.samal918@gmail.com>
2021-06-21 21:20:21 +0530

    feat: Add Projects Page

---
+++ pages/works.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Layout, Container, Cards } from '../components';
+import { getContentList } from '../lib/content';
+
+/**
+ * Work page `/work`
+ */
+const Work = ({ works }) => {
+  return (
+    <Layout
+      pathname={'/works'}
+      pageTitle='Works &amp; Projects'
+      pageDescription='Works and projects spanning Product design, Research, frontend and software engineering with ReactJS, React Native and NodeJs'
+    >
+      <Container>
+        {/* <p className='page-intro'>
+          
+        </p> */}
+        <Cards data={works} basePath='works' />
+      </Container>
+    </Layout>
+  );
+};
+
+export const getStaticProps = async () => {
+  const works = await getContentList('work');
+  return {
+    props: { works },
+  };
+};
+
+export default Work;
