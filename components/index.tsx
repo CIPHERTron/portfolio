5d3dbdfa2026926c415aaff2c7a4462766eb5632
CIPHERTron <pritish.samal918@gmail.com>
2021-06-21 21:24:45 +0530

    feat: Use Named Exports to export Components

---
+++ components/index.tsx
@@ -0,0 +1,9 @@
+export * from './layout';
+export * from './container';
+export * from './cards';
+
+export { default as Blogs } from './blogs';
+export { default as AboutSection } from './about';
+export { default as SkillsSection } from './skils';
+export { default as Chip } from './chip';
+export { OutLineBtn } from './button';
