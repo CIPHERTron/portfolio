e366201137b5adc3761f0f7b40be5e614afa3e55
CIPHERTron <pritish.samal918@gmail.com>
2021-06-21 21:29:29 +0530

    feat: Add Brand Logo For Future Use[Navbar]

---
+++ components/logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { StyledLogo } from '../styles/logo.styles';
+
+const Logo = () => {
+  return <StyledLogo src='/logo.png' alt='logo' />;
+};
+
+export default Logo;
