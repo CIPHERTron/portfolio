b4c1b89d9e796cfbc0d166d8bd9b1d00b53b1d3c
CIPHERTron <pritish.samal918@gmail.com>
2021-06-21 21:44:21 +0530

    chore: Dynamically Render md files as separate blogs

---
+++ pages/articles/[id].tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+
+import { Layout, Container } from '../../components';
+import { getAllContentIds, getContentData } from '../../lib/content';
+import { StyledContent } from '../../styles/content.styles';
+import { Chips } from '../../components/chips/chips';
+
+/**
+ *  Renders articles markdown posts
+ */
+
+const Article = ({ articlesData }: { articlesData: IContentData }) => {
+  const { pathname } = useRouter();
+  const { title, contentHtml, description } = articlesData;
+
+  return (
+    <Layout pathname={pathname} pageTitle={title} pageDescription={description}>
+      <Container width='narrow'>
+        <StyledContent>
+          <time>{articlesData.date}</time>
+          {articlesData.previewImage && (
+            <Image src={articlesData.previewImage} height={550} width={1200} />
+          )}
+          <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+          {articlesData.tags && <Chips items={articlesData.tags} />}
+        </StyledContent>
+      </Container>
+    </Layout>
+  );
+};
+
+export const getStaticPaths = async () => {
+  const paths = getAllContentIds('articles');
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export interface IContentData {
+  id: string;
+  contentHtml: string;
+  date: Date;
+  title: string;
+  previewImage?: string;
+  description?: string;
+  tags?: string[];
+  category?: string;
+}
+
+export const getStaticProps = async ({ params }) => {
+  const articlesData: IContentData = await getContentData(params.id, 'articles');
+  return {
+    props: {
+      articlesData,
+    },
+  };
+};
+
+export default Article;
