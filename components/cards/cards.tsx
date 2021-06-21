63969bb992ece848e794b56c33631ca296d2d48e
CIPHERTron <pritish.samal918@gmail.com>
2021-06-21 21:13:31 +0530

    chore: Render markdown through project cards

---
+++ components/cards/cards.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
+import { GitHub, ExternalLink } from 'react-feather';
+
+import { StyledCards, ProjectSection, Wrapper, Heading } from '../../styles/cards.styles';
+
+interface ICard {
+  basePath: string;
+  data: {
+    title: string;
+    href: string;
+    github: string;
+    id: string;
+    slug: string;
+    date: Date;
+    previewImage: string;
+    description: string;
+  }[];
+}
+
+/**
+ * Renders a grid of cards
+ * @param {Array} data Data to display in grid
+ */
+
+const Cards = ({ data, basePath }: ICard) => {
+  const router = useRouter();
+
+  return (
+    <ProjectSection>
+      <Heading>Work & Experience</Heading>
+      <StyledCards>
+        {data.map((singleCard) => (
+          <article className='article' key={singleCard.id}>
+            {/* @ts-ignore */}
+            <Image
+              src={singleCard.previewImage}
+              alt={singleCard.title}
+              width={450}
+              height={220}
+              sizes='(min-width: 640px) 700px, 400px'
+            />
+            <Wrapper style={{ marginTop: '10px' }}>
+              <time>{singleCard.date}</time>
+              <Wrapper>
+                <a
+                  href={singleCard.href}
+                  onClick={(e) => {
+                    e.preventDefault;
+                    router.push(singleCard.href);
+                  }}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <GitHub className='one logo' />
+                </a>
+                <a
+                  href={singleCard.github}
+                  onClick={(e) => {
+                    e.preventDefault;
+                    router.push(singleCard.github);
+                  }}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  <ExternalLink className='two logo' />
+                </a>
+              </Wrapper>
+            </Wrapper>
+            <Link href={`/${basePath}/[id]`} as={`/${basePath}/${singleCard.slug}`}>
+              <a>
+                <h2>{singleCard.title}</h2>
+                {singleCard.description && <p>{singleCard.description}</p>}
+              </a>
+            </Link>
+          </article>
+        ))}
+      </StyledCards>
+    </ProjectSection>
+  );
+};
+
+export { Cards };
---
+++ styles/cards.styles.ts
@@ -0,0 +1,131 @@
+import styled from '@emotion/styled';
+
+export const ProjectSection = styled.section`
+  padding-left: 12%;
+  padding-right: 12%;
+`;
+
+export const Heading = styled.h2`
+  color: #fff;
+  text-align: center;
+  font-size: 64px;
+
+  @media (max-width: 1024px) {
+    font-size: 48px;
+  }
+  @media (max-width: 769px) {
+    font-size: 32px;
+  }
+`;
+
+export const StyledCards = styled.section`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+
+  @media (max-width: 1300px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+  @media (max-width: 769px) {
+    grid-template-columns: repeat(1, 1fr);
+  }
+
+  h2 {
+    color: var(--text-color);
+  }
+  p {
+    color: var(--text-color-dark);
+  }
+
+  article.article {
+    margin: 0 2% 2%;
+    overflow: hidden;
+
+    &:hover h2 {
+      color: #fff;
+    }
+    &:hover p {
+      color: var(--text-color-bright);
+    }
+
+    img {
+      transition: all var(--animation-duration) var(--transition-timing-function);
+      object-fit: cover;
+    }
+
+    &:hover img {
+      transform: scale(1.1);
+    }
+
+    .logo {
+      margin-left: 7px;
+      height: 20px;
+    }
+
+    &:hover .one {
+      color: var(--text-color);
+    }
+
+    @media all and (min-width: 560px) {
+      margin-right: 0.5em;
+
+      &:last-child {
+        margin-right: 0;
+      }
+    }
+
+    @media all and (min-width: 1024px) {
+      flex: 0 1 32.5%;
+      img {
+        /* height: 180px; */
+      }
+    }
+  }
+
+  @media all and (min-width: 560px) {
+    justify-content: space-between;
+    flex-direction: row;
+  }
+
+  time {
+    color: #808080;
+    font-size: 0.9em;
+  }
+
+  a {
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: none;
+    }
+
+    &:hover h2 {
+      text-decoration: underline;
+      color: #fff;
+    }
+  }
+
+  h2 {
+    font-size: 1.17em;
+    margin-bottom: 5px;
+  }
+
+  img {
+    width: 100%;
+    object-fit: cover;
+    margin-bottom: 0.4em;
+    transform: scale(1);
+    transition: 5s all;
+  }
+
+  @media all and (min-width: 560px) {
+    justify-content: flex-start;
+    flex-direction: row;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
