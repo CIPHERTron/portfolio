2d01db6ede9c0e7e2018765185fa5435438f9b64
CIPHERTron <pritish.samal918@gmail.com>
2021-06-24 18:32:45 +0530

    feat: Add Sample Photography Page

--- pages/photography.tsx
+++ pages/photography.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
-function Photography() {
+import { Layout, Container } from '../components';
+const Photography = ({ articles }) => {
   return (
-    <div>
-      <h1>Welcome to my Photography Page</h1>
-    </div>
+    <Layout
+      pathname={'/photography'}
+      pageTitle='Photography'
+      pageDescription='Showcasing some of my best shots till date.'
+    >
+      <Container>
+        <p className='page-intro'>
+          I've a knack of Photography and I click on my Mobile. So, here are some shots that I've
+          clicked. PS: I don't have a DSLR :P'
+        </p>
+
+        <h2 style={{ textAlign: 'center' }}>Photography Page Coming Soon!</h2>
+      </Container>
+    </Layout>
   );
-}
+};
 
 export default Photography;
