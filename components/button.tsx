fe1339c07527dee6e39edf5864c9c0ce063979bb
CIPHERTron <pritish.samal918@gmail.com>
2021-06-25 00:24:15 +0530

    feat: Add Explore Buttons

--- components/button.tsx
+++ components/button.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
-import { OutlinedButton } from '../styles/buttons.styles';
+import { OutlinedButton, ButtonWrapper } from '../styles/buttons.styles';
 
 export const OutLineBtn = ({ label }) => <OutlinedButton>{label}</OutlinedButton>;
+
+export const ExploreButton = ({ label, href }) => (
+  <ButtonWrapper>
+    <Link href={href} as={href}>
+      <a>
+        <OutlinedButton>{label}</OutlinedButton>
+      </a>
+    </Link>
+  </ButtonWrapper>
+);
