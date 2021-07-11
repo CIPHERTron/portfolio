d3fba834bef7eeefb450cfc20c39dd6f41b06218
CIPHERTron <pritish.samal918@gmail.com>
2021-07-12 02:37:55 +0530

    static: Add Experience List | GDSC NITR

---
+++ config/experience.ts
@@ -0,0 +1,18 @@
+export const List = [
+  {
+    heading: 'Core Team, GDSC NIT Rourkela',
+    logo: 'gdsc.png',
+    src: 'https://dscnitrourkela.org/',
+    alt: 'GDSC NIT Rourkela',
+    desc: `Google Developer Student Club is a Google Developers program intended as a space for
+ students to try out new ideas and collaborate to solve mobile and web development problems.`,
+  },
+  {
+    heading: 'Core Team, GDSC NIT Rourkela',
+    logo: 'gdsc.png',
+    src: 'https://dscnitrourkela.org/',
+    alt: 'GDSC NIT Rourkela',
+    desc: `Google Developer Student Club is a Google Developers program intended as a space for
+ students to try out new ideas and collaborate to solve mobile and web development problems.`,
+  },
+];
