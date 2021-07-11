ee7c2fa3effa15a600a7d553cd93425539e07248
CIPHERTron <pritish.samal918@gmail.com>
2021-07-12 03:15:44 +0530

    static: Add Opencode, OBF Experience

--- config/experience.ts
+++ config/experience.ts
@@ -1,6 +1,13 @@
 export const List = [
   {
-    heading: 'Core Team, GDSC NIT Rourkela',
+    heading: `Backend Developer, OBF (Jun'21 - Aug'21)`,
+    logo: 'obf.png',
+    src: 'https://www.open-bio.org/',
+    alt: 'Open Bioinformatics Foundation',
+    desc: `The Open Bioinformatics Foundation (OBF) is a non-profit, volunteer-run group that promotes open source software development and Open Science within the biological research community. `,
+  },
+  {
+    heading: `Core Team, GDSC NIT Rourkela (Dec'20 - NOW)`,
     logo: 'gdsc.png',
     src: 'https://dscnitrourkela.org/',
     alt: 'GDSC NIT Rourkela',
@@ -8,11 +15,10 @@ export const List = [
  students to try out new ideas and collaborate to solve mobile and web development problems.`,
   },
   {
-    heading: 'Core Team, GDSC NIT Rourkela',
-    logo: 'gdsc.png',
-    src: 'https://dscnitrourkela.org/',
-    alt: 'GDSC NIT Rourkela',
-    desc: `Google Developer Student Club is a Google Developers program intended as a space for
- students to try out new ideas and collaborate to solve mobile and web development problems.`,
+    heading: `Core Team, OpenCode NIT Rourkela (Nov'20 - NOW)`,
+    logo: 'opencode.png',
+    src: 'https://opencodenitr.herokuapp.com/',
+    alt: 'Opencode NIT Rourkela',
+    desc: `OpenCode is the open source community of NIT Rourkela. It a community of student developers/coders, dedicated to contributing to the open-source community through the skills they acquire in our learning process.`,
   },
 ];
