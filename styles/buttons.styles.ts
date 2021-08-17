bd050d71bfc839f156eaeb597789abc8783c0af2
Pritish Samal <pritish.samal918@gmail.com>
2021-08-17 13:46:52 +0530

    Update buttons.styles.ts

--- styles/buttons.styles.ts
+++ styles/buttons.styles.ts
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const OutlinedButton = styled.button`
   padding: 10px 7px;
-  border: 0.1em solid #ffffff;
+  border: 0.2em solid #ffffff;
   background: inherit;
   margin: 0 0.3em 0.3em 0;
   border-radius: 0.12em;
